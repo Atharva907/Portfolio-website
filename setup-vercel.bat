192a8942e4f0786d7a2afc5f334be8511e984807
Atharva Chavan <atharva.chavan907@gmail.com>
2025-10-24 22:40:19 +0530

    full website

--- setup-vercel.bat
+++ setup-vercel.bat
@@ -1,11 +1,14 @@
 
+
 @echo off
 echo Setting up project structure for Vercel deployment...
 
 REM Create app directory if it doesn't exist
 if not exist "app" mkdir app
 
-REM Copy all necessary files from src/app to app
+REM Create components directory in app if it doesn't exist
+if not exist "app\components" mkdir app\components
+
 echo Copying layout.js...
 copy "srcpp\layout.js" "app\layout.js" /Y
 
@@ -15,15 +18,15 @@ copy "srcpp\page.js" "app\page.js" /Y
 echo Copying globals.css...
 copy "srcpp\globals.css" "app\globals.css" /Y
 
-echo Copying favicon.ico...
-copy "srcppavicon.ico" "appavicon.ico" /Y
-
-REM Create components directory in app if it doesn't exist
-if not exist "app\components" mkdir app\components
-
+REM Copy component files
 echo Copying component files...
 copy "srcpp\components\*.jsx" "app\components" /Y
+
+REM Copy favicon.ico using xcopy to handle binary files
+echo Copying favicon.ico...
+xcopy "srcppavicon.ico" "appavicon.ico" /Y /I
 
 echo Project structure updated successfully!
 echo You can now run 'npm run build' to test the build locally.
 pause
+
