7ef91da8d5ed02a7ad28c07838309f2500c22bce
Atharva Chavan <atharva.chavan907@gmail.com>
2025-10-25 00:35:28 +0530

    contact working

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 使用标准配置
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
